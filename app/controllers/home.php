6ec711f75d04fdc311315839e2e6733f345a1141
Mustafa Elazazy <75970269+melazazy@users.noreply.github.com>
2024-07-09 23:05:52 +0300

    Update home.php

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -10,6 +10,8 @@ class Home extends Controller
         $products = $this->loadmodel('Product');
         $data['banner'] = $banner->get3Banners();
         $data['cats'] = $cat->get3Categories();
+        echo $data['cats'];
+        die;
         $data['products'] = $products->get3FeaturedProducts();
         $this->view("zay_shop/index", $data);
     }
